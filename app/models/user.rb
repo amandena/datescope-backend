ce0944b05ab70e3789fba795088276a3cc14b291
amandena <manda.panda91@yahoo.com>
2019-11-20 23:13:26 -0500

    added validations and methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :matches
+
+  validates :email, uniqueness: true
+  validates :email, :name, :age, :sign, :bio, :gender, :preference, :location, :social_media, presence: true
+  validates_inclusion_of :gender, :in => ['male', 'female']
+  validates_inclusion_of :preference, :in => ['male', 'female', 'both']
+
+  def find_matches(location, gender, sign)
+    if self.location === location
+      if self.preference === 'both'
+        if self.preference === gender
+          if self.sign_compatability(sign)
+
+          end
+        end
+      end
+    end
+  end
+
+  def sign_compatability(other_sign)
+
+  end
 end
