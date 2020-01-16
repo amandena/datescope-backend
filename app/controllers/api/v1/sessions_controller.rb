0a882e4f609fba7464c3e0ca1cd5b485f955d4eb
amandena <manda.panda91@yahoo.com>
2020-01-15 20:58:03 -0500

    added logic to application controller actions, added get_current_user route, added credentials/cookie functionality to app

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,6 +8,14 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {error: 'Not logged in.'}
+    end
+  end
+
   def destroy
   end
 end
