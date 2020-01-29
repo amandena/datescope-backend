57a43bb3920fab44f440bfaa9069ec848c0cc0f6
amandena <manda.panda91@yahoo.com>
2020-01-29 00:41:33 -0500

    added signup route and refactored users controller create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,9 +18,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render json: @user
     else
       render json: {error: 'Well, that was weird. Did you miss something? Please try again.'}
+
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/signup', to: 'users#create'
       post '/login', to: 'sessions#create'
       get '/get_current_user', to: 'sessions#get_current_user'
       delete '/logout', to: 'sessions#destroy'
